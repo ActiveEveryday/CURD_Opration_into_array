4df988206b3845552a433a39a2d79c9b97f0fcec
Mohit Paddhariya <mpaddhariya435@rku.ac.in>
2022-09-04 02:08:39 +0530

    pop.c is insert and push.c is modifyed

--- pop.c
+++ pop.c
@@ -3,14 +3,15 @@
 
 void main()
 {
-    int i;
-    int user;
-    printf("Enter the how many element you want into the array : ");
+    int user, i;
+    printf("Enter the how many elements you want into the array : ");
     scanf("%d", &user);
-    int arr[user];
+    int max = user;
+    int arr[user], pos;
+
     for (i = 0; i < user; i++)
     {
-        printf("Enter %d element: ", i + 1);
+        printf("Enter the %d element: ", i + 1);
         scanf("%d", &arr[i]);
     }
     printf("\nYou Enterd into array is: \n");
@@ -18,24 +19,16 @@ void main()
     {
         printf("%d ", arr[i]);
     }
-
-    int pos, value;
-
-    printf("\n\nEnter which Position you want insert new element : ");
+    printf("\n\nEnter which position you want to delete ( note : position start's with 1 ): ");
     scanf("%d", &pos);
-    if (pos < user && pos >= 0)
+    if (pos > 0 && pos <= user)
     {
-        printf("Enter which new position value : ");
-        scanf("%d", &value);
-
-        for (i = user - 1; i >= pos; i--)
+        for (i = pos; i < user; i++)
         {
-            arr[i + 1] = arr[i];
+            arr[i] = arr[i + 1];
         }
-        arr[pos] = value;
-
         printf("\nAfter entering a new element your element of and array is : \n");
-        for (i = 0; i < user + 1; i++)
+        for (i = 0; i < user - 1; i++)
         {
             printf("%d ", arr[i]);
         }
